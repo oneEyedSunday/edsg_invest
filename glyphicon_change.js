d2c248571770ce1d543ffbad2e6c7db2509476f0
Idiakose Sunday <idiakosesunday@gmail.com>
2017-11-13 22:29:18 +0100

    first commit

---
+++ glyphicon_change.js
@@ -0,0 +1,6 @@
+$('.collapse').on('shown.bs.collapse', function(){
+  console.log($(this));
+$(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus").closest(".panel-heading").addClass("active-panel");
+}).on('hidden.bs.collapse', function(){
+$(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus").closest(".panel-heading").removeClass("active-panel");
+});
